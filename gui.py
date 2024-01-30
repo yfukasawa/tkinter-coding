ec8b0d5732ecb8a5647a7624c71e3198821ae267
yfukasawa <gairoju@gmail.com>
2024-01-30 11:42:42 +0900

    ボールを追加

--- gui.py
+++ gui.py
@@ -4,6 +4,21 @@ import time
 window_width  = 700
 window_height = 500
 
+canvas_width  = 600
+canvas_height = 400
+
+# co: circle_object
+co = {
+    "x": 350,
+    "y": 250,
+    "dx": 10,
+    "dy": 10,
+    "r": 30
+}
+
+def create_circle(canvas, x, y, r, **kwargs):
+    return canvas.create_oval(x-r, y-r, x+r, y+r, **kwargs)
+
 def main():
     window = tk.Tk()
     window.geometry("%dx%d" % (window_width, window_height))
@@ -13,6 +28,11 @@ def main():
 
     label = tk.Label(window, text="Hello World!")
     label.place(x=0, y=0)
+
+    canvas = tk.Canvas(window, width=canvas_width, height=canvas_height, bg="white")
+    canvas.place(x=(window_width-canvas_width)/2, y=(window_height-canvas_height)/2)
+
+    circle = create_circle(canvas, co["x"], co["y"], co["r"], fill="blue")
 
     window.mainloop()
 
